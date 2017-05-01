5ad2366fa34ae5edb4ccba97ecde3ff088ace4fa
dergey <dergey@gmail.com>
2017-05-01 16:24:17 +0300

    Рафакторил

---
+++ app/src/main/java/com/zhuravlev/sergey/remindme/dto/LotDTO.java
@@ -0,0 +1,46 @@
+package com.zhuravlev.sergey.remindme.dto;
+
+public class LotDTO {
+
+    private String title, description;
+    private Double price;
+    private String currency = "BYN";
+
+    public LotDTO(String title, String description, Double price) {
+        this.title = title;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+}
